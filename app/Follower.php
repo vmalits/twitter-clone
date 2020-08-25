be89d11d843f3be1b6345d74e8b7497bf8768b9a
Владимир Малиц <vladimir.malits@gmail.com>
2020-08-25 13:24:50 +0300

    added laravel ide helper,generated meta files

--- app/Follower.php
+++ app/Follower.php
@@ -4,6 +4,24 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Follower
+ *
+ * @property int $id
+ * @property int $user_id
+ * @property int $following_id
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|Follower newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Follower newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Follower query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Follower whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Follower whereFollowingId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Follower whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Follower whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Follower whereUserId($value)
+ * @mixin \Eloquent
+ */
 class Follower extends Model
 {
     //
